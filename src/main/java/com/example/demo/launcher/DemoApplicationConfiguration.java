040b75f39fb576bbb00ff86d20cb9b9eeb10984b
Akshay <akshaykolhe1809@gmail.com>
2017-08-15 02:59:22 +0530

    First commit

---
+++ src/main/java/com/example/demo/launcher/DemoApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.demo.launcher;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories("com.example.demo.repo")
+@EntityScan("com.example.demo.entity")
+public class DemoApplicationConfiguration {
+
+}
